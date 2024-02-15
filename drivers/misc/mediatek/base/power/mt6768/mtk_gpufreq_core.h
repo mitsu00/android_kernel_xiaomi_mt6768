19ffe11728fcdedaf90d922385ac11fca322853b
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2024-02-15 16:03:11 +0000

    update gpu freq to 1.15ghz

--- drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
+++ drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
@@ -17,12 +17,12 @@
 /**************************************************
  * MT6768/MT6767 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG_GPU_DVFS_FREQ0			(1000000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ1			(975000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ2			(950000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ3			(925000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ4			(900000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ5			(875000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ0			(1150000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ1			(1100000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ2			(1050000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ3			(1000000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ4			(950000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ5			(900000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ6			(850000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ7			(823000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ8			(796000)	/* KHz */
