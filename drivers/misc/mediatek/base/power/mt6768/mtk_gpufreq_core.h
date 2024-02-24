35456d7ea706ae2604c47c047c3c6c70ec9bd718
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2024-02-24 09:03:33 +0000

    adjust gpu clock + power

--- drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
+++ drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
@@ -17,12 +17,12 @@
 /**************************************************
  * MT6768/MT6767 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG_GPU_DVFS_FREQ0			(1150000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ1			(1100000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ2			(1050000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ3			(1000000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ4			(950000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ5			(900000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ0			(1100000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ1			(1050000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ2			(1000000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ3			(950000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ4			(920000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ5			(880000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ6			(850000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ7			(823000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ8			(796000)	/* KHz */
@@ -50,12 +50,12 @@
 #define SEG_GPU_DVFS_FREQ30			(315000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ31			(299000)	/* KHz */
 
-#define SEG_GPU_DVFS_VOLT0		(95000)		/* mV x 100 */
-#define SEG_GPU_DVFS_VOLT1		(92500)		/* mV x 100 */
-#define SEG_GPU_DVFS_VOLT2		(90000)		/* mV x 100 */
-#define SEG_GPU_DVFS_VOLT3		(87500)		/* mV x 100 */
-#define SEG_GPU_DVFS_VOLT4		(85000)		/* mV x 100 */
-#define SEG_GPU_DVFS_VOLT5		(82500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT0		(97000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT1		(94000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT2		(91000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT3		(88000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT4		(86000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT5		(83000)		/* mV x 100 */
 #define SEG_GPU_DVFS_VOLT6		(80000)		/* mV x 100 */
 #define SEG_GPU_DVFS_VOLT7		(79375)		/* mV x 100 */
 #define SEG_GPU_DVFS_VOLT8		(78125)		/* mV x 100 */
